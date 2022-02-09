084eeb8537d240511d7e034ef892b277c6547131
truong202 <38mavantruong12a4@gmail.com>
2022-02-09 20:10:47 +0700

    update

---
+++ ProgramCreateATMCard.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+#include "ConsoleUI.c"
+#include "Function.c"
+#include "Model.c"
+
+int main(int argc, char const *argv[])
+{
+    char isContinue = 'Y';
+    do
+    {
+        ATMCard card = inputCard();
+        int result = createATMCard(card);
+        printf(" Create ATM %s!\n", result == 1 ? "completed" : "not complete");
+        printf("Do you want to create ATM Card? (Y/N): ");
+        isContinue = getYN();
+    } while (isContinue == 'Y' || isContinue == 'y');
+
+    return 0;
+}
